2ff870261fe07e9ba0dca058b6fa6b47cdbf398e
FernandoFigueroa0710 <fernandoffigueroa@gmail.com>
2019-02-15 23:54:48 -0600

    on press function delete item on the list working

--- src/components/PlaceList/PlaceList.js
+++ src/components/PlaceList/PlaceList.js
@@ -7,7 +7,7 @@ const PlaceList = props => {
     <ListItem
       key={i}
       placeName={place}
-      onItemPressed={() => alert("Item Pressed - ID:" + i)}
+      onItemPressed={() => props.onItemDeleted(i)}
     />
   ));
   return <View style={styles.listContainer}>{placesOutput}</View>;
