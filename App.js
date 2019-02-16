0bfc7441d26c06ad6fe770b6578d2f6e2e2918b6
FernandoFigueroa0710 <fernandoffigueroa@gmail.com>
2019-02-15 20:01:58 -0600

    button rendering text input in the state

--- App.js
+++ App.js
@@ -7,37 +7,67 @@
  * @lint-ignore-every XPLATJSCOPYRIGHT1
  */
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View, TextInput, Button} from 'react-native';
+import React, { Component } from "react";
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Button
+} from "react-native";
 
 const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+  ios: "Press Cmd+R to reload,\n" + "Cmd+D or shake for dev menu",
   android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
+    "Double tap R on your keyboard to reload,\n" +
+    "Shake or press menu button for dev menu"
 });
 
 type Props = {};
 export default class App extends Component<Props> {
   state = {
-    placeName: ''
-  }
+    placeName: "",
+    places: []
+  };
 
   placeNameChangeHandler = event => {
-   this.setState({placeName: event})
-  }
+    this.setState({ placeName: event });
+  };
+
+  placeSubmitHandler = () => {
+    if (this.state.placeName.trim() === "") {
+      return;
+    } else {
+      this.setState(prevState => {
+        return {
+          places: prevState.places.concat(prevState.placeName)
+        };
+      });
+    }
+  };
+
   render() {
+    const placesOutput = this.state.places.map((place, i) => (
+      <Text key={i}>{place}</Text>
+    ));
     return (
       <View style={styles.container}>
         <View style={styles.inputContainer}>
           <TextInput
-          style={styles.textInput}
-          placeholder= "An Awesome place"
-          value={this.state.placeName}
-          onChangeText={this.placeNameChangeHandler}
-            />
-            <Button title="Add" style={styles.button}/>
-          </View>
+            style={styles.textInput}
+            placeholder="An Awesome place"
+            value={this.state.placeName}
+            onChangeText={this.placeNameChangeHandler}
+          />
+          <Button
+            title="Add"
+            style={styles.button}
+            onPress={this.placeSubmitHandler}
+          />
+        </View>
+        {placesOutput}
+        <View />
       </View>
     );
   }
@@ -47,16 +77,16 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 50,
-    justifyContent: 'flex-start',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF'
+    justifyContent: "flex-start",
+    alignItems: "center",
+    backgroundColor: "#F5FCFF"
   },
   inputContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center'
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center"
   },
-  textInput :{
+  textInput: {
     width: "70%"
   },
   button: {
