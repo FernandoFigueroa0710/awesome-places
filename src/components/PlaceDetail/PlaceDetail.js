f79e0cff6c3a71ad9d852cdf3675c9374f15f7a3
FernandoFigueroa0710 <fernandoffigueroa@gmail.com>
2019-02-18 21:56:48 -0600

    switch to the conditional view for the odal

--- src/components/PlaceDetail/PlaceDetail.js
+++ src/components/PlaceDetail/PlaceDetail.js
@@ -1,17 +1,43 @@
 import React from "react";
-import { Modal, View, Image, Text, Button } from "react-native";
+import { Modal, View, Image, Text, Button, StyleSheet } from "react-native";
 
-const PlaceDetail = props => (
-  <Modal>
-    <View>
-      <Image />
-      <Text />
+const PlaceDetail = props => {
+  let modalContent = null;
+
+  if (props.selectedPlace) {
+    modalContent = (
       <View>
-        <Button />
-        <Button />
+        <Image source={props.selectedPlace.image} style={styles.placeImage} />
+        <Text style={styles.placeName}>{props.selectedPlace.name}</Text>
       </View>
-    </View>
-  </Modal>
-);
+    );
+  }
+  return (
+    <Modal visible={props.selectedPlace !== null} anymationType="slide">
+      <View style={styles.modalContainer}>
+        {modalContent}
+        <View>
+          <Button title="Delete" color="red" />
+          <Button title="Close" />
+        </View>
+      </View>
+    </Modal>
+  );
+};
 
-export default PLaceDetail;
+const styles = StyleSheet.create({
+  modalContainer: {
+    margin: 35
+  },
+  placeImage: {
+    width: "100%",
+    height: 200
+  },
+  placeName: {
+    fontWeight: "bold",
+    textAlign: "center",
+    fontSize: 30
+  }
+});
+
+export default PlaceDetail;
