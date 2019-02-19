821fedb9cc81e8c4316804210cef638b480c450b
FernandoFigueroa0710 <fernandoffigueroa@gmail.com>
2019-02-18 21:06:03 -0600

    created new component for modal

---
+++ src/components/PlaceDetail/PlaceDetail.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Modal, View, Image, Text, Button } from 'react-native';
+
+const PlaceDetail = props => (
+  <Modal>
+    <View>
+      <Image/>
+      <Text></Text>
+      <View>
+        <Button />
+        <Button />
+      </View>
+    </View>
+  </Modal>
+);
+
+export defualt PLaceDetail;
