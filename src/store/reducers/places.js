af88859a84419b00c0b6d90fc9c6fc3594f6b2ff
FernandoFigueroa0710 <fernandoffigueroa@gmail.com>
2019-02-20 20:04:25 -0600

    create the reducer directory with the places file to mananage the state

---
+++ src/store/reducers/places.js
@@ -0,0 +1,13 @@
+const initialSatate = {
+  places: [],
+  selectedPlace: null
+};
+
+const reducer = (state = initialSatate, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+export default reducer;
