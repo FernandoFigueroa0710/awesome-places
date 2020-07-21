e76c975913b0b870789377b5a6d5f854054b4feb
FernandoFigueroa0710 <fernandoffigueroa@gmail.com>
2020-07-20 22:16:35 -0500

    Update

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
-import { AppRegistry } from "react-native";
-import App from "./App";
+import React from 'react';
+import { AppRegistry } from 'react-native';
+import { Provider } from 'react-redux';
+import App from './App';
+import configureStore from './src/store/configureStore';
 
-AppRegistry.registerComponent("AwesomePlaces", () => App);
+const store = configureStore();
+
+const RNRedux = () => (
+    <Provider store={store}>
+        <App />
+    </Provider>
+);
+
+AppRegistry.registerComponent('rncourse', () => RNRedux);
